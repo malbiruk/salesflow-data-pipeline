33a3432eadf3d6a5230ec1b6bf4a846cce437004
malbiruk <2601074@gmail.com>
2025-04-09 14:44:41 +0400

    created a db schema

---
+++ db/sales_records.sql
@@ -0,0 +1,33 @@
+CREATE TABLE region (region VARCHAR(255) PRIMARY KEY);
+
+CREATE TABLE country (
+    country VARCHAR(255) PRIMARY KEY,
+    region VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE product (
+    item_type VARCHAR(255) PRIMARY KEY,
+    unit_cost FLOAT,
+    unit_price FLOAT
+);
+
+CREATE TABLE order_priority (order_priority VARCHAR(255) PRIMARY KEY);
+
+CREATE TABLE orders (
+    id INTEGER,
+    country VARCHAR(255) NOT NULL,
+    is_online BOOLEAN,
+    order_priority VARCHAR(255) NOT NULL,
+    product_id VARCHAR(255),
+    units_sold INTEGER,
+    order_date DATE,
+    ship_date DATE
+);
+
+ALTER TABLE country ADD FOREIGN KEY (region) REFERENCES region (region);
+
+ALTER TABLE orders ADD FOREIGN KEY (country) REFERENCES country (country);
+
+ALTER TABLE orders ADD FOREIGN KEY (order_priority) REFERENCES order_priority (order_priority);
+
+ALTER TABLE orders ADD FOREIGN KEY (product_id) REFERENCES product (item_type);
